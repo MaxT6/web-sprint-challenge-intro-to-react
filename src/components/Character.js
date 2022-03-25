624f8e5edff380b5947845d40750310b51a87be1
Maximilian Thiel <maximilianthiel@maximilians-mbp.myfiosgateway.com>
2022-03-25 13:10:36 -0400

    installed axios, and styled-components. Created skeleton for Characters module.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+
+
+const Character = props => {
+    return (
+        <div>
+            {props}
+        </div>
+    )
+}
+
+
+
+export default Character
